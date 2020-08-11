72e0a541f532166b4d08e52f3aff0980f6c00f2d
czpla <edu.czpla@gmail.com>
2020-08-11 17:07:52 -0300

    new changes

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './style.css'
 
 function TeacherList () {
     return (
-        <h1>TeacherList</h1>
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject" />
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da semana</label>
+                        <input type="text" id="week_day" />
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time" />
+                    </div>
+                </form>
+            </PageHeader>
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars1.githubusercontent.com/u/53007583?s=460&u=e2af5ec14151de239ec40f401a58819413cfd675&v=4" alt="Eduardo Czpla"/>
+                        <div>
+                            <strong>Eduardo Czpla</strong>
+                            <span>Química</span>
+                        </div>
+                    </header>
+                    <p>
+                        Estusiasta das melhores tecnologias de química avançada.
+                        <br/><br/>
+                        Apaixonado por explodir coisas em laborartório e por mudar a vida das pessoas através de experiencias.
+                    </p>
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 20,00</strong>
+                        </p>
+                        <button type="button"> 
+                            <img src={whatsappIcon} alt="Whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
+        </div>
     );
 }
 
